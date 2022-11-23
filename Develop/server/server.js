14b03782c16f15f0ea58480980e95471e8480117
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-23 10:31:25 -0500

    deleted install in pacakge json to see if deployment will work

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -12,3 +12,4 @@ require('./routes/htmlRoutes')(app);
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 // moved package.json files out of routes folder
 // added json descriptions into code!
+// deleted "install": "cd server && npm i && cd ../client && npm i", in pacakage json to see if deployment will work on heroku
